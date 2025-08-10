3e7e123b902fbaebccf452eccf9f5b2fa399983c
Katya Titkova <katyatitkova@gmail.com>
2025-08-10 11:05:08 +0200

    add the row ruler

--- rulerGenerator.js
+++ rulerGenerator.js
@@ -1,8 +1,6 @@
 //TODO adjust constants
 //TODO adjust labels positions
 //TODO draw every marking if it's possible
-//TODO fix right border
-//TODO add a second ruler for row gauge
 //TODO vertical labels for row ruler
 //TODO square option
 //TODO update readme
@@ -11,6 +9,7 @@
 
 const leftMargin = 30
 const rightMargin = 60
+const betweenRulers = 100
 
 const drawDpi = 300
 const pdfDpi = 72
@@ -19,28 +18,51 @@ const pixelsPerCm = drawDpi / cmPerInch
 const markingLengthMultiplier = 0.25
 const markingLengthCm = 0.8
 const markingLengthPixels = markingLengthCm * pixelsPerCm
+const fontSize = 26
+const rulerWidthCm = 3
+const rulerWidthPixels = pixelsPerCm * rulerWidthCm
 
 const canvasElementId = "rulerCanvas"
 
-const ruler = {
-    lengthPixels: pixelsPerCm * 18,
-    widthPixels: pixelsPerCm * 3,
-    fontSize: 26
+const stsRuler = {
+    lengthPixels: pixelsPerCm * 25,
+    widthPixels: rulerWidthPixels,
+    unit: "stitches",
+    startX: 0,
+    startY: 0,
+}
+
+const rowsRuler = {
+    lengthPixels: pixelsPerCm * 25,
+    widthPixels: rulerWidthPixels,
+    unit: "rows",
+    startX: 0,
+    startY: rulerWidthPixels + betweenRulers,
 }
 
 const updateVariables = function () {
-    ruler.scale = document.getElementById("scale").value
-    ruler.sts = document.getElementById("sts").value
-    ruler.stsCm = document.getElementById("stsCm").value
-    ruler.gauge = ruler.sts / ruler.stsCm
-    ruler.lengthCm = ruler.lengthPixels / pixelsPerCm * ruler.scale
-    ruler.lengthStitches = ruler.lengthCm * ruler.gauge
-    ruler.pixelsPerStitch = pixelsPerCm / ruler.gauge
+    stsRuler.sts = document.getElementById("sts").value
+    stsRuler.stsCm = document.getElementById("stsCm").value
+    stsRuler.scale = document.getElementById("scale").value
+
+    stsRuler.gauge = stsRuler.sts / stsRuler.stsCm
+    stsRuler.lengthCm = stsRuler.lengthPixels / pixelsPerCm * stsRuler.scale
+    stsRuler.lengthStitches = stsRuler.lengthCm * stsRuler.gauge
+    stsRuler.pixelsPerStitch = pixelsPerCm / stsRuler.gauge
+
+    rowsRuler.sts = document.getElementById("rows").value
+    rowsRuler.stsCm = document.getElementById("rowsCm").value
+    rowsRuler.scale = document.getElementById("scale").value
+
+    rowsRuler.gauge = rowsRuler.sts / rowsRuler.stsCm
+    rowsRuler.lengthCm = rowsRuler.lengthPixels / pixelsPerCm * rowsRuler.scale
+    rowsRuler.lengthStitches = rowsRuler.lengthCm * rowsRuler.gauge
+    rowsRuler.pixelsPerStitch = pixelsPerCm / rowsRuler.gauge
 }
 
 const resizeCanvas = function () {
-    let width = ruler.lengthPixels + rightMargin
-    let height = ruler.widthPixels
+    let width = stsRuler.lengthPixels + rightMargin + 1
+    let height = rowsRuler.startY + rowsRuler.widthPixels + 1
 
     document.getElementById(canvasElementId).width = width
     document.getElementById(canvasElementId).height = height
@@ -49,7 +71,7 @@ const resizeCanvas = function () {
     document.getElementById(canvasElementId).style.height = (height / 3) + 'px'
 }
 
-const addMarkingLabel = function (x1, y2, isFinal, label) {
+const addMarkingLabel = function (ruler, x, y, isFinal, label) {
     let xLabelOffset = -15
     let yLabelOffset = 30
 
@@ -57,7 +79,7 @@ const addMarkingLabel = function (x1, y2, isFinal, label) {
         xLabelOffset = -1 * xLabelOffset
     }
 
-    let text = new paper.PointText(new paper.Point(x1 + xLabelOffset, y2 + yLabelOffset))
+    let text = new paper.PointText(new paper.Point(x + xLabelOffset, y + yLabelOffset))
     text.justification = "left"
     if (isFinal) { // last label is right justified
         text.justification = "right"
@@ -69,24 +91,24 @@ const addMarkingLabel = function (x1, y2, isFinal, label) {
     text.style = {
         fontFamily: "monospace",
         fontWeight: "bold",
-        fontSize: ruler.fontSize
+        fontSize: fontSize
     }
-    text.name = "Label no. " + label
+    text.name = ruler.unit + " label no. " + label
 }
 
-const drawMarking = function (index, markingLength, spacing, assignNumber, isFinal) {
-    let x1 = leftMargin + (spacing * index)
+const drawMarking = function (ruler, index, markingLength, spacing, assignNumber, isFinal) {
+    let x1 = ruler.startX + leftMargin + (spacing * index)
     let x2 = x1
-    let y1 = 0
-    let y2 = markingLength
+    let y1 = ruler.startY
+    let y2 = ruler.startY + markingLength
 
     let line = new paper.Path.Line([x1, y1], [x2, y2])
-    line.name = "Marking no. " + index
+    line.name = ruler.unit + " marking no. " + index
     line.strokeColor = "black"
     line.strokeWidth = "1"
 
     if (assignNumber) {
-        addMarkingLabel(x1, y2, isFinal, index)
+        addMarkingLabel(ruler, x1, y2, isFinal, index)
     }
 }
 
@@ -97,31 +119,31 @@ const drawBorder = function (name, x1, y1, x2, y2) {
     topLine.strokeWidth = "1"
 }
 
-const drawBorders = function () {
-    let width = ruler.lengthPixels + rightMargin
-    let height = ruler.widthPixels
-    drawBorder("top", 0, 0, width, 0)
-    drawBorder("bottom", 0, height, width, height)
-    drawBorder("left", 0, 0, 0, height)
-    drawBorder("right", width, 0, width, height)
+const drawBorders = function (ruler) {
+    ruler.endX = ruler.startX + ruler.lengthPixels + rightMargin
+    ruler.endY = ruler.startY + ruler.widthPixels
+    drawBorder(ruler.unit + " top", ruler.startX, ruler.startY, ruler.endX, ruler.startY)
+    drawBorder(ruler.unit + " bottom", ruler.startX, ruler.endY, ruler.endX, ruler.endY)
+    drawBorder(ruler.unit + " left", ruler.startX, ruler.startY, ruler.startX, ruler.endY)
+    drawBorder(ruler.unit + " right", ruler.endX, ruler.startY, ruler.endX, ruler.endY)
 }
 
-const addRulerInfo = function () {
-    let text = new paper.PointText(new paper.Point(100, ruler.widthPixels - 100))
+const addRulerInfo = function (ruler) {
+    let text = new paper.PointText(new paper.Point(ruler.startX + 100, ruler.startY + ruler.widthPixels - 100))
     text.justification = "left"
 
     text.fillColor = "black"
-    text.content = "Scale 1/" + ruler.scale + ", gauge " + ruler.sts + " sts per " + ruler.stsCm + " cm"
+    text.content = "Scale 1/" + ruler.scale + ", gauge " + ruler.sts + " " + ruler.unit + " per " + ruler.stsCm + " cm"
 
     text.style = {
         fontFamily: "monospace",
         fontWeight: "bold",
-        fontSize: ruler.fontSize
+        fontSize: fontSize
     }
-    text.name = "Ruler info"
+    text.name = ruler.unit + " ruler info "
 }
 
-const constructRuler = function () {
+const constructRuler = function (ruler) {
     let isFinal = false
     let distance = 2
     for (let i = 0; i <= ruler.lengthStitches; i += distance) {
@@ -134,10 +156,10 @@ const constructRuler = function () {
         if (i === ruler.lengthStitches) {
             isFinal = true
         }
-        drawMarking(i, markingLength, spacing, assignNumber, isFinal)
+        drawMarking(ruler, i, markingLength, spacing, assignNumber, isFinal)
     }
-    drawBorders()
-    addRulerInfo()
+    drawBorders(ruler)
+    addRulerInfo(ruler)
 }
 
 const build = function () {
@@ -149,7 +171,8 @@ const build = function () {
     if (form.reportValidity()) {
         updateVariables()
         resizeCanvas()
-        constructRuler()
+        constructRuler(stsRuler)
+        constructRuler(rowsRuler)
 
         paper.view.draw()
     }
